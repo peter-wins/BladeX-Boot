d3efc953a39c6c5f123f625d6edf40907ae59e43
Abert <1255343742@qq.com>
2021-05-14 10:27:19 +0400

    test

--- src/main/java/org/springblade/modules/system/controller/LogApiController.java
+++ src/main/java/org/springblade/modules/system/controller/LogApiController.java
@@ -49,7 +49,7 @@ public class LogApiController {
 	private final ILogApiService logService;
 
 	public static void main(String[] args) {
-		System.out.println("我是Abert");
+		System.out.println("我是Abert perfect");
 	}
 
 	/**
