bf8d399178973df252e64f064da2e418c21afe83
Abert <1255343742@qq.com>
2021-05-23 15:27:15 +0400

    租客池优化

--- src/main/java/org/springblade/modules/tenantpool/mapper/tenantpool.sql
+++ src/main/java/org/springblade/modules/tenantpool/mapper/tenantpool.sql
@@ -1,10 +1,10 @@
 INSERT INTO `blade_menu`(`id`, `parent_id`, `code`, `name`, `alias`, `path`, `source`, `sort`, `category`, `action`, `is_open`, `remark`, `is_deleted`)
-VALUES ('1395750987393667078', 0, 'tenantpool', 'tenantpool', 'menu', '/tenantpool/tenantpool', NULL, 1, 1, 0, 1, NULL, 0);
+VALUES ('1396423649631670279', 0, 'tenantpool', 'tenantpool', 'menu', '/tenantpool/tenantpool', NULL, 1, 1, 0, 1, NULL, 0);
 INSERT INTO `blade_menu`(`id`, `parent_id`, `code`, `name`, `alias`, `path`, `source`, `sort`, `category`, `action`, `is_open`, `remark`, `is_deleted`)
-VALUES ('1395750987393667079', '1395750987393667078', 'tenantpool_add', '新增', 'add', '/tenantpool/tenantpool/add', 'plus', 1, 2, 1, 1, NULL, 0);
+VALUES ('1396423649631670280', '1396423649631670279', 'tenantpool_add', '新增', 'add', '/tenantpool/tenantpool/add', 'plus', 1, 2, 1, 1, NULL, 0);
 INSERT INTO `blade_menu`(`id`, `parent_id`, `code`, `name`, `alias`, `path`, `source`, `sort`, `category`, `action`, `is_open`, `remark`, `is_deleted`)
-VALUES ('1395750987393667080', '1395750987393667078', 'tenantpool_edit', '修改', 'edit', '/tenantpool/tenantpool/edit', 'form', 2, 2, 2, 1, NULL, 0);
+VALUES ('1396423649631670281', '1396423649631670279', 'tenantpool_edit', '修改', 'edit', '/tenantpool/tenantpool/edit', 'form', 2, 2, 2, 1, NULL, 0);
 INSERT INTO `blade_menu`(`id`, `parent_id`, `code`, `name`, `alias`, `path`, `source`, `sort`, `category`, `action`, `is_open`, `remark`, `is_deleted`)
-VALUES ('1395750987393667081', '1395750987393667078', 'tenantpool_delete', '删除', 'delete', '/api/tenantpool/tenantpool/remove', 'delete', 3, 2, 3, 1, NULL, 0);
+VALUES ('1396423649631670282', '1396423649631670279', 'tenantpool_delete', '删除', 'delete', '/api/tenantpool/tenantpool/remove', 'delete', 3, 2, 3, 1, NULL, 0);
 INSERT INTO `blade_menu`(`id`, `parent_id`, `code`, `name`, `alias`, `path`, `source`, `sort`, `category`, `action`, `is_open`, `remark`, `is_deleted`)
-VALUES ('1395750987393667082', '1395750987393667078', 'tenantpool_view', '查看', 'view', '/tenantpool/tenantpool/view', 'file-text', 4, 2, 2, 1, NULL, 0);
+VALUES ('1396423649631670283', '1396423649631670279', 'tenantpool_view', '查看', 'view', '/tenantpool/tenantpool/view', 'file-text', 4, 2, 2, 1, NULL, 0);
