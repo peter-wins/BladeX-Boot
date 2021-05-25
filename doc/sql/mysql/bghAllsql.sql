454c39cb1d1dddf1d371b233854b6665f5e31ac5
Abert <1255343742@qq.com>
2021-05-25 14:28:32 +0400

    跟进记录sql

--- doc/sql/mysql/bghAllsql.sql
+++ doc/sql/mysql/bghAllsql.sql
@@ -234,4 +234,36 @@ CREATE TABLE `bgh_after_service` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='租后服务表';
 
 
+DROP TABLE IF EXISTS `bgh_tenant_followup`;
+CREATE TABLE `bgh_tenant_followup` (
+                                       `id` bigint(64) NOT NULL COMMENT '主键id',
+                                       `tenantpool_id` bigint(64) NOT NULL COMMENT '租客池id',
+                                       `followup_content` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL COMMENT '跟进内容',
+                                       `create_user` bigint(64) DEFAULT NULL COMMENT '创建人',
+                                       `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                                       `update_user` bigint(64) DEFAULT NULL COMMENT '修改人',
+                                       `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+                                       `create_dept` bigint(64) DEFAULT NULL COMMENT '创建部门',
+                                       `status` int(2) NOT NULL DEFAULT '0' COMMENT '(跟进状态)1：正常,2:关闭',
+                                       `is_deleted` int(2) DEFAULT '0' COMMENT '是否已删除',
+                                       PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='租客池跟进记录表';
+
+
+DROP TABLE IF EXISTS `bgh_owner_followup`;
+CREATE TABLE `bgh_owner_followup` (
+                                      `id` bigint(64) NOT NULL COMMENT '主键id',
+                                      `ownerpool_id` bigint(64) NOT NULL COMMENT '业主池id',
+                                      `followup_content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '跟进内容',
+                                      `create_user` bigint(64) DEFAULT NULL COMMENT '创建人',
+                                      `create_time` datetime DEFAULT NULL COMMENT '创建时间',
+                                      `update_user` bigint(64) DEFAULT NULL COMMENT '修改人',
+                                      `update_time` datetime DEFAULT NULL COMMENT '修改时间',
+                                      `create_dept` bigint(64) DEFAULT NULL COMMENT '创建部门',
+                                      `status` int(2) NOT NULL DEFAULT '0' COMMENT '(跟进状态)1：正常,2:关闭',
+                                      `is_deleted` int(2) DEFAULT '0' COMMENT '是否已删除',
+                                      PRIMARY KEY (`id`) USING BTREE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci ROW_FORMAT=COMPACT COMMENT='业主池跟进记录表';
+
+
 SET FOREIGN_KEY_CHECKS = 1;
